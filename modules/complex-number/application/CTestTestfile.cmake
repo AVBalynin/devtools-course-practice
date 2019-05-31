8aa2547ad2944bea2feefcd5b693a9d67bc3c912
AVBalynin <alexbalynin@gmail.com>
2019-05-31 19:41:42 +0300

    fix 0.2

--- modules/complex-number/application/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/A.V.Balynin/Desktop/PAPKA1/devtools-course-practice/modules/complex-number/application
-# Build directory: C:/Users/A.V.Balynin/Desktop/PAPKA1/modules/complex-number/application
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
